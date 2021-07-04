d8cc8932f5074e88227b8fe7dfed85a9863c3e11
dedo <MRolqueza@gmail.com>
2021-07-04 00:30:16 -0400

    Vistas de preencvio

---
+++ application/views/cuestionarios/vreforma_preenvio.php
@@ -0,0 +1,53 @@
+<main>
+	<br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+				<div class="card">
+					<div class="card-header cuest1" >
+						Reforma Electoral
+					</div>
+					<div class="card-body" >Contenido</div>
+					<div class="card-footer" >
+						<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Enviar</button>
+						<a href="#" class="btn btn-info" role="button">Cancelar</a>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+</main>
+
+
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+
+			<!-- Modal Header -->
+			<div class="modal-header">
+				<h4 class="modal-title">Modal Heading</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			</div>
+
+			<!-- Modal body -->
+			<div class="modal-body">
+				Enviar Informacion
+			</div>
+
+			<!-- Modal footer -->
+			<div class="modal-footer">
+				<!-- Al metodo de insercion -->
+				<a href="#" class="btn btn-info" role="button">Si</a>
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+			</div>
+
+		</div>
+	</div>
+</div>
