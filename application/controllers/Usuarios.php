fa951c16e296f2c9b8a67dace3d6a8d4f33c11ec
dedo <MRolqueza@gmail.com>
2021-07-06 13:11:18 -0400

    Modificacion de datos y pass de Usuario implementada

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -88,6 +88,39 @@ class Usuarios extends CI_Controller
 		$this->load->view('html/pie');
 	}
 
+	public function procesarUsrLog()
+	{
+		$idusuario = $this->input->post('idusuario');
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('nombre', 'Nombre', 'required');
+		$this->form_validation->set_rules('apellido', 'Apellido', 'required');
+		$this->form_validation->set_rules('carnet', 'Carnet de Identidad', 'numeric');
+		if($this->form_validation->run()==false)
+		{
+			$usuario = $this->ion_auth->user($idusuario)->row();
+			$data['usuario'] = $usuario;
+			$data['grupos'] = $this->ion_auth->groups()->result();
+			$data['departamentos'] = $this->Departamento_model->leerDepartamentos();
+			$this->load->view('usuarios/vformulario_monitor_editar', $data);
+		}else{
+			/** @noinspection PhpLanguageLevelInspection */
+			$datos_extra = [
+				'first_name' => $this->input->post('nombre'),
+				'last_name' => $this->input->post('apellido'),
+				'carnet_identidad' => $this->input->post('carnet'),
+				'geolocalizacion' => $this->input->post('ubicacion'),
+				'direccion' => $this->input->post('direccion'),
+			];
+
+			if($this->ion_auth->update($idusuario, $datos_extra)){
+				echo "Usuario modificado";
+			}else{
+				echo "Usuario no modificado";
+			}
+		}
+
+	}
+
 	public function procesarEditar()
 	{
 		$idusuario = $this->input->post('idusuario');
@@ -104,6 +137,7 @@ class Usuarios extends CI_Controller
 			$data['departamentos'] = $this->Departamento_model->leerDepartamentos();
 			$this->load->view('usuarios/vformulario_usuario_editar', $data);
 		}else{
+			/** @noinspection PhpLanguageLevelInspection */
 			$datos_extra = [
 				'first_name' => $this->input->post('nombre'),
 				'last_name' => $this->input->post('apellido'),
@@ -120,6 +154,50 @@ class Usuarios extends CI_Controller
 			}
 		}
 
+
+	}
+
+	public function editarPwd()
+	{
+		$usuario = $this->ion_auth->user()->row();
+		$data['usuario'] = $usuario;
+
+		$this->load->view('html/encabezado');
+		$this->load->view('html/navbar');
+		$this->load->view('usuarios/vformulario_pwd', $data);
+		$this->load->view('html/pie');
+
+	}
+
+	public function procesarEditPwd()
+	{
+		$idusuario = $this->input->post('idusuario');
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('pass', 'Contraseña', 'required');
+		$this->form_validation->set_rules('pass1', 'Confirmar Contraseña', 'required|matches[pass]');
+
+
+		if($this->form_validation->run()==false)
+		{
+			$usuario = $this->ion_auth->user($idusuario)->row();
+			$data['usuario'] = $usuario;
+			$this->load->view('html/encabezado');
+			$this->load->view('html/navbar');
+			$this->load->view('usuarios/vformulario_pwd', $data);
+			$this->load->view('html/pie');
+		}else{
+			/** @noinspection PhpLanguageLevelInspection */
+			$datos_extra = [
+				'password' => $this->input->post('pass'),
+			];
+
+			if($this->ion_auth->update($idusuario, $datos_extra)){
+				//echo "Contraseña modificada";
+				redirect('inicio');
+			}else{
+				//echo "Contraseña no modificado";
+			}
+		}
 
 	}
 
--- application/views/usuarios/vformulario_monitor_editar.php
+++ application/views/usuarios/vformulario_monitor_editar.php
@@ -2,9 +2,64 @@
 	<br>
 	<div class="container" >
 		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 color-contenedores Caja_de_datos">
 
+				<div id="esquinas_redondeadas">
+					<div id="Caja_de_orden" class="">
+						<h3 class="text-center" >
+							Modificar Datos del Usuario: <?php echo $usuario->username; ?>
+						</h3>
+					</div>
+					<hr>
+					<div id="Caja_de_datos" class="">
+						<?php echo form_open('usuarios/procesarUsrLog');?>
+						<div class="form-group">
+							<label for="nombre" >Nombre<span class="text-danger"> *</span></label>
+							<input id="nombre" name="nombre" class="form-control cuadro"
+								   value="<?php echo $usuario->first_name;?>"  required >
+							<input type="hidden" id="idusuario" name="idusuario"
+								   value="<?php echo $usuario->id;?>" >
+						</div>
+						<div class="form-group">
+							<label for="apellido" >Apellido<span class="text-danger"> *</span></label>
+							<input id="apellido" name="apellido" class="form-control cuadro"
+								   value="<?php echo $usuario->last_name;?>" required>
+						</div>
+						<div class="form-group">
+							<label for="apellido" >Carnet de Identidad</label>
+							<input id="carnet" name="carnet" class="form-control cuadro"
+								   value="<?php echo $usuario->carnet_identidad;?>" >
+						</div>
+						<!--<div class="form-group">
+							<label for="" >Departamento</label>
+							<select id="departamento" name="departamento" class="form-control" >
+								<option value="1">La Paz</option>
+								<option value="2">Oruro</option>
+							</select>
+						</div>-->
+						<div class="form-group">
+							<label for="direccion">Direccion</label>
+							<input id="direccion" name="direccion" class="form-control cuadro"
+								   value="<?php echo $usuario->direccion;?>" >
+						</div>
+						<div class="form-group">
+							<label for="ubicacion">Ubicacion</label>
+							<input id="ubicacion" name="ubicacion" class="form-control cuadro"
+								   value="<?php echo $usuario->geolocalizacion; ?>"  >
+						</div>
+						<div class="form-group">
+							<button type="submit" class="boton btn btn-primary">Editar</button>
+							<a href="<?php echo site_url('/')?>" class="btn btn-danger botonsimple " role="button" >Cancelar</a>
+						</div>
+						<?php echo form_close(); ?>
+						<br>
+					</div>
+
+				</div>
+
+			</div>
 		</div>
 	</div>
 
-
+<br>
 </main>
