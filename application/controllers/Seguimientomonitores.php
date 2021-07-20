184cffd64c135043f65a93e6b7645893c1e57d69
will <olivguil9@gmail.com>
2021-07-19 21:39:04 -0400

    210719 seguimiento monitores

--- application/controllers/Seguimientomonitores.php
+++ application/controllers/Seguimientomonitores.php
@@ -25,30 +25,22 @@ class Seguimientomonitores extends CI_Controller
         $TablaAuxiliarSm->nombre=0;
         foreach ($DatosSeguimientoM['SeguimientoMonitores'] as $f)
         {
-            echo"<pre>";echo 'foreach1***********';echo"</pre>";
-            echo ($f->first_name);
-            echo ($TablaAuxiliarSm->nombre);
             if($f->first_name === $TablaAuxiliarSm->nombre)
             {
-                echo"<pre>";echo'if1si***********';echo"</pre>";
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $nDc0)
                 {
-                    echo"<pre>";echo'foreach2***********';echo"</pre>";
                     $cuestionarioA=$nDc0->nombre_cuestionario;
-//                    $feecha='fecha '.$cuestionarioA;
+                    $feechaa='fecha '.$cuestionarioA;
                     if($f->nombre_cuestionario === $nDc0->nombre_cuestionario)
                     {
-                        echo"<pre>";echo'if2si***********';echo"</pre>";
-//                        $TablaAuxiliarSm->$feecha=$f->fecha_registro;
+                        $TablaAuxiliarSm->$feechaa=$f->fecha_registro;
                         $TablaAuxiliarSm->$cuestionarioA=$f->ncuestionario;
                     }
                     else
                     {
-                        echo"<pre>";echo'if2no***********';echo"</pre>";
-                        if(/*$TablaAuxiliarSm->$feecha === 0 &&*/ $TablaAuxiliarSm->$cuestionarioA === 0)
+                        if(/*$TablaAuxiliarSm->$feechaa === 0 &&*/ $TablaAuxiliarSm->$cuestionarioA === 0)
                         {
-                            echo"<pre>";echo 'if3no***********';echo"</pre>";
-//                            $TablaAuxiliarSm->$feecha=0;
+                            $TablaAuxiliarSm->$feechaa=0;
                             $TablaAuxiliarSm->$cuestionarioA=0;
                         }
                     }
@@ -56,35 +48,28 @@ class Seguimientomonitores extends CI_Controller
             }
             else
             {
-                echo"<pre>";echo 'if1no*********** CREA TABLA';echo"</pre>";
                 $TablaAuxiliarSm=new stdClass();
                 $TablaAuxiliarSm->nombre=$f->first_name;
                 $TablaAuxiliarSm->apellido=$f->last_name;
                 $TablaAuxiliarSm->departamento=$f->nombre_departamento;
-//                echo"<pre>";var_dump($TablaAuxiliarSm);echo"</pre>";
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $ndc1)
                 {
-                    echo"<pre>";echo 'foreach3***********';echo"</pre>";
                     $cuestionario1=$ndc1->nombre_cuestionario;
                     $TablaAuxiliarSm->$cuestionario1=0;
                 }
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $nDc)
                 {
-                    echo"<pre>";echo 'foreach4***********';echo"</pre>";
                     $cuestionarioA=$nDc->nombre_cuestionario;
 //                    $feecha='fecha '.$cuestionarioA;
                     if($f->nombre_cuestionario === $nDc->nombre_cuestionario)
                     {
-                        echo"<pre>";echo 'if4si***********';echo"</pre>";
 //                        $TablaAuxiliarSm->$feecha=$f->fecha_registro;
                         $TablaAuxiliarSm->$cuestionarioA=$f->ncuestionario;
                     }
                     else
                     {
-                        echo"<pre>"; echo 'if4no***********'; echo"</pre>";
                         if(/*$TablaAuxiliarSm->$feecha === 0 &&*/ $TablaAuxiliarSm->$cuestionarioA === 0)
                         {
-                            echo"<pre>"; echo 'if5no***********'; echo"</pre>";
 //                            $TablaAuxiliarSm->$feecha=0;
                             $TablaAuxiliarSm->$cuestionarioA=0;
                         }
