cfd24336da68609ac245a78ba51f95575c6bcd30
will <olivguil9@gmail.com>
2021-07-22 19:17:56 -0400

    adicion de consulta seguimientomonitoresdocentes

--- application/models/SeguimientoMonitores_model.php
+++ application/models/SeguimientoMonitores_model.php
@@ -17,6 +17,20 @@ class SeguimientoMonitores_model extends CI_Model{
                 . "GROUP BY first_name, nombre_cuestionario");
         return $qry->result();
     }
+    public function leerSeguimientoMonitoresDocentes()
+    {
+        $qry=$this->db->query("SELECT first_name,last_name,nombre_departamento,fecha_registro,nombre_cuestionario,count(*) ncuestionario "
+                . "FROM noticia "
+                . "LEFT JOIN users "
+                . "ON users.id = noticia.rel_idusuario "
+                . "LEFT JOIN cuestionario "
+                . "ON noticia.rel_idcuestionario = cuestionario.idcuestionario "
+                . "LEFT JOIN departamento "
+                . "ON departamento.iddepartamento = users.rel_iddepartamento "
+                . "WHERE iddepartamento = 2 "
+                . "GROUP BY first_name, nombre_cuestionario");
+        return $qry->result();
+    }
     public function leerCuestionarios()
     {
         $qry=$this->db->query("SELECT * "
