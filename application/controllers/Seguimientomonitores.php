99db37d20961ab4205adabc571d923efe7df5bec
will <olivguil9@gmail.com>
2021-07-24 00:59:46 -0400

    210724 limpieza seguimientomonitores

--- application/controllers/Seguimientomonitores.php
+++ application/controllers/Seguimientomonitores.php
@@ -198,18 +198,14 @@ class Seguimientomonitores extends CI_Controller
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $nDc0)
                 {
                     $cuestionarioA=$nDc0->nombre_cuestionario;
-//                    $feechaa='fecha '.$cuestionarioA;
                     if($f->nombre_cuestionario == $nDc0->nombre_cuestionario)
                     {
-//                        $TablaAuxiliarSm->$feechaa=date('d-m-Y',$f->fecha_registro);
                         $TablaAuxiliarSm->$cuestionarioA=$f->ncuestionario;
                     }
                     else
                     {
-                        //var_dump($TablaAuxiliarSm->$feechaa);
-                        if(/*$TablaAuxiliarSm->$feechaa == 0 &&*/ $TablaAuxiliarSm->$cuestionarioA == 0)
+                        if($TablaAuxiliarSm->$cuestionarioA == 0)
                         {
-//                            $TablaAuxiliarSm->$feechaa=0;
                             $TablaAuxiliarSm->$cuestionarioA=0;
                         }
                     }
@@ -219,8 +215,6 @@ class Seguimientomonitores extends CI_Controller
             {
                 $TablaAuxiliarSm=new stdClass();
                 $TablaAuxiliarSm->nombre=$f->first_name;
-//                $TablaAuxiliarSm->apellido=$f->last_name;
-//                $TablaAuxiliarSm->departamento=$f->nombre_departamento;
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $ndc1)
                 {
                     $cuestionario1=$ndc1->nombre_cuestionario;
@@ -229,18 +223,14 @@ class Seguimientomonitores extends CI_Controller
                 foreach($DatosSeguimientoM['NumeroDeCuestionarios'] as $nDc)
                 {
                     $cuestionarioA=$nDc->nombre_cuestionario;
-//                    $feecha='fecha '.$cuestionarioA;
                     if($f->nombre_cuestionario == $nDc->nombre_cuestionario)
                     {
-//                        $TablaAuxiliarSm->$feecha=date('d-m-Y',$f->fecha_registro);
                         $TablaAuxiliarSm->$cuestionarioA=$f->ncuestionario;
                     }
                     else
                     {
-                        //var_dump($TablaAuxiliarSm->$feechaa);
-                        if(/*$TablaAuxiliarSm->$feecha == 0 &&*/ $TablaAuxiliarSm->$cuestionarioA == 0)
+                        if($TablaAuxiliarSm->$cuestionarioA == 0)
                         {
-//                            $TablaAuxiliarSm->$feecha=0;
                             $TablaAuxiliarSm->$cuestionarioA=0;
                         }
                     }
@@ -251,14 +241,12 @@ class Seguimientomonitores extends CI_Controller
         $tablaSm['SeguimientoM1']=$SeguimientoM;
         $tablaSm['NumeroDeCuestionarios']=$DatosSeguimientoM['NumeroDeCuestionarios'];
 /*        echo "<pre>"; var_dump($tablaSm); echo "</pre>";*/
- /*       echo "<pre>"; var_dump($DatosSeguimientoM['SeguimientoMonitores']); echo "</pre>";*/
         if(empty($tablaSm))
         {
         redirect('Seguimientomonitores');
         }
          else
          {
-        //var_dump($DatosSeguimientoM);
         $this->load->view('html/encabezado');
         $this->load->view('html/navbar');
         $this->load->view('seguimientom/vseguimientoMonitoresTabla1',$tablaSm);
