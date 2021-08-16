04d8fa23868a015520d29941dbc1b264e92f8e14
dedo <MRolqueza@gmail.com>
2021-08-16 09:36:20 -0400

    Arreglos de vistas

--- application/views/subtema/vcrearsubtema.php
+++ application/views/subtema/vcrearsubtema.php
@@ -1,27 +1,39 @@
+<main role="main">
 	<br>
-	<div id="esquinas_redondeadas">		
-		<div id="Caja_de_orden" class="Caja_de_datos">
-			<h3 id="Título_central"> Crear Nuevo SubTema</h3>
-		</div>
-		<div id="Caja_de_datos" class="Caja_de_datos">
-			<form action="<?php echo base_url().'index.php/SubTema/agregarSubTema'; ?>" method="post">
-				<label for="nombre_subtema" class="form-group"> Nombre de SubTema </label> 
-				<span class="rojo"> * </span>
-				<br>
-				<input type="text" id="cuadro" name="nombre_subtema"  required>
-				<br><br>
-				<label for="idtema" class="form-group"> Escoger Tema al que pertenece el SubTema</label> 
-				<span class="rojo"> * </span>
-				<br>
-				<select name="idtema" id="idtema">
-					<option value="0"></option>
-					<?php foreach ($temas as $t) {?>
-						<option value="<?php echo $t->idtema;?>"><?php echo $t->nombre_tema;?></option>
-					<?php } ?>
-				</select>
-				<br><br>
-			    <input type="submit" id="BOTON" value="CREAR">
-				<a href="<?php echo site_url('SubTema/');?>"><input type="button" class="BOTONROJO" value="CANCELAR"></a>
-			</form>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 color-contenedores">
+				<div id="esquinas_redondeadas">
+					<div id="Caja_de_orden" class="Caja_de_datos">
+						<h3 id="Título_central"> Crear Nuevo SubTema</h3>
+					</div>
+					<div id="Caja_de_datos" class="Caja_de_datos">
+						<form action="<?php echo base_url().'index.php/SubTema/agregarSubTema'; ?>" method="post">
+							<label for="nombre_subtema" class="form-group"> Nombre de SubTema </label>
+							<span class="rojo"> * </span>
+							<br>
+							<input type="text" id="cuadro" name="nombre_subtema"  required>
+							<br><br>
+							<label for="idtema" class="form-group"> Escoger Tema al que pertenece el SubTema</label>
+							<span class="rojo"> * </span>
+							<br>
+							<select name="idtema" id="idtema">
+								<option value="0"></option>
+								<?php foreach ($temas as $t) {?>
+									<option value="<?php echo $t->idtema;?>"><?php echo $t->nombre_tema;?></option>
+								<?php } ?>
+							</select>
+							<br><br>
+							<input type="submit" id="BOTON" value="CREAR">
+							<a href="<?php echo site_url('SubTema/');?>"><input type="button" class="BOTONROJO" value="CANCELAR"></a>
+						</form>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	</div>
+	<br>
+</main>
+
+
