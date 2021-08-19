43faae2d4401663d02cd11648b5f20f235713f80
dedo <MRolqueza@gmail.com>
2021-08-19 15:52:48 -0400

    Reportes leyes, mas correcciones

--- application/views/manejodb/viniciodb.php
+++ application/views/manejodb/viniciodb.php
@@ -42,7 +42,7 @@
 					</div>
 				</div>
 			</div>
-
+			<?php endif; ?>
 			<div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
 				<div class="card">
 					<div class="card-header" style="background-color:#7f182b;color:white">
@@ -57,7 +57,7 @@
 				</div>
 			</div>                    
 
-			<?php endif; ?>
+
 
 		</div>
 	</div>
