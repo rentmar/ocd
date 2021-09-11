cb2af1ab7d56c957a5e8b4c3b295416a9b557aac
will <olivguil9@gmail.com>
2021-09-10 23:32:10 -0400

    2109101131 graficoRadial

--- application/models/Radial_model.php
+++ application/models/Radial_model.php
@@ -6,107 +6,70 @@ class Radial_model extends CI_Model
         parent::__construct();
         $this->load->database();
     }
-    public function leerMactores($fecha)
+    public function leerMactores($fechai,$fechaf)
     {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
+        $fecha_inicio = $fechai;
+        $fecha_fin = $fechaf;
         		$sql = "SELECT idactor, nombre_actor, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
                                 . "FROM actor "
                                 . "LEFT JOIN noticia_actor ON noticia_actor.rel_idactor = actor.idactor "
                                 . "LEFT JOIN noticia ON noticia.idnoticia = noticia_actor.rel_idnoticia "
                                 . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
+                                . "WHERE (noticia.fecha_registro BETWEEN ? AND ?) "
                                 . "GROUP BY idactor, idcuestionario";
-                $qry = $this->db->query($sql);
+                $qry = $this->db->query($sql, [$fecha_inicio, $fecha_fin ]);
 		return $qry->result();
     }
-    public function leerMmedioDcomunicacion($fecha)
+    public function leerMmedioDcomunicacion($fechai,$fechaf)
     {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
+        $fecha_inicio = $fechai;
+        $fecha_fin = $fechaf;
                 $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, rel_idtipomedio, COUNT(*) ncuestionario "
                 . "FROM noticia "
                 . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
                 . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
+                . "WHERE (noticia.fecha_registro BETWEEN ? AND ?) "
                 . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
+                $qry = $this->db->query($sql,[$fecha_inicio, $fecha_fin]);
         return $qry->result();
     }
-    public function leerMCredSocial($fecha)
+    public function leerMCcanalDtv($fechai,$fechaf)
     {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
+        $fecha_inicio = $fechai;
+        $fecha_fin = $fechaf;
                 $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
                 . "FROM noticia "
                 . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
                 . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 1 "
+                . "WHERE medio_comunicacion.rel_idtipomedio = 2 AND (noticia.fecha_registro BETWEEN ? AND ?) "
                 . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
+                $qry = $this->db->query($sql,[$fecha_inicio, $fecha_fin]);
         return $qry->result();
     }
-    public function leerMCcanalDtv($fecha)
+    public function leerMCemisoraRadial($fechai,$fechaf)
     {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
+        $fecha_inicio = $fechai;
+        $fecha_fin = $fechaf;
                 $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
                 . "FROM noticia "
                 . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
                 . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 2 "
+                . "WHERE medio_comunicacion.rel_idtipomedio = 3 AND (noticia.fecha_registro BETWEEN ? AND ?) "
                 . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
+                $qry = $this->db->query($sql,[$fecha_inicio, $fecha_fin]);
         return $qry->result();
     }
-    public function leerMCemisoraRadial($fecha)
+    public function leerMCprensaEscrita($fechai,$fechaf)
     {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
+        $fecha_inicio = $fechai;
+        $fecha_fin = $fechaf;
                 $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
                 . "FROM noticia "
                 . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
                 . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 3 "
+                . "WHERE medio_comunicacion.rel_idtipomedio = 4 AND (noticia.fecha_registro BETWEEN ? AND ?) "
                 . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
-        return $qry->result();
-    }
-    public function leerMCprensaEscrita($fecha)
-    {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
-                $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
-                . "FROM noticia "
-                . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
-                . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 4 "
-                . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
-        return $qry->result();
-    }
-    public function leerMCtvRural($fecha)
-    {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
-                $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
-                . "FROM noticia "
-                . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
-                . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 5 "
-                . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
-        return $qry->result();
-    }
-        public function leerMCradioRural($fecha)
-    {
-        $fecha_inicio = $fecha->fecha_inicio;
-        $fecha_fin = $fecha->fecha_fin;
-                $sql = "SELECT idmedio, nombre_medio, idcuestionario, nombre_cuestionario, COUNT(*) ncuestionario "
-                . "FROM noticia "
-                . "LEFT JOIN medio_comunicacion ON medio_comunicacion.idmedio = noticia.rel_idmedio "
-                . "LEFT JOIN cuestionario ON cuestionario.idcuestionario = noticia.rel_idcuestionario "
-                . "WHERE medio_comunicacion.rel_idtipomedio = 6 "
-                . "GROUP BY idmedio, idcuestionario";
-                $qry = $this->db->query($sql);
+                $qry = $this->db->query($sql,[$fecha_inicio, $fecha_fin]);
         return $qry->result();
     }
 }
