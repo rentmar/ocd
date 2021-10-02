885458e3cefc39c949f2208a117ac5e0e9459d8d
dedo <MRolqueza@gmail.com>
2021-10-02 13:01:09 -0400

    Vista para encuestadores habilitada

--- application/views/html/pie.php
+++ application/views/html/pie.php
@@ -10,6 +10,8 @@
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js"></script>
+
 
 
 
