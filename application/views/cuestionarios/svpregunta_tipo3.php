a25c895db95deb5cd607e729d762e84568633de5
dedo <MRolqueza@gmail.com>
2023-08-05 07:19:37 -0400

    Bug pregunta, seleccion de  opcion triple corregido

--- application/views/cuestionarios/svpregunta_tipo3.php
+++ application/views/cuestionarios/svpregunta_tipo3.php
@@ -10,7 +10,7 @@
 		<label class="custom-control-label" for="<?php echo 'opcion2'.$pregunta->idpregunta; ?>">No</label>
 	</div>
 	<div class="custom-control custom-radio">
-		<input type="radio" class="custom-control-input" id="<?php echo 'opcion1'.$pregunta->idpregunta; ?>" name="<?php echo 'pregunta'.$pregunta->codigo_pregunta; ?>" value="algunos">
-		<label class="custom-control-label" for="<?php echo 'opcion1'.$pregunta->idpregunta; ?>">Algunos</label>
+		<input type="radio" class="custom-control-input" id="<?php echo 'opcion3'.$pregunta->idpregunta; ?>" name="<?php echo 'pregunta'.$pregunta->codigo_pregunta; ?>" value="algunos">
+		<label class="custom-control-label" for="<?php echo 'opcion3'.$pregunta->idpregunta; ?>">Algunos</label>
 	</div>
 </div>
