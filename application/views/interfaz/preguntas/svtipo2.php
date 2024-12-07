962c62e41eeee93d2fbc1f446b6bd607cfc15d89
dedo <MRolqueza@gmail.com>
2024-12-07 12:10:44 -0400

    Preguntas ajustadas

--- application/views/interfaz/preguntas/svtipo2.php
+++ application/views/interfaz/preguntas/svtipo2.php
@@ -3,16 +3,25 @@
 		<?php echo $etiqueta.'. '.$pregunta; ?>
 	</label><br>
 	<div class="custom-control custom-radio custom-control-inline">
-		<input type="radio" class="custom-control-input" id="pregunta_csej13a" name="pregunta_csej13" value="1">
-		<label class="custom-control-label" for="pregunta_csej13a">Si</label>
+		<input type="radio" class="custom-control-input"
+			   id="pregunta<?php echo $idpregunta;?>a" name="pregunta<?php echo $idpregunta;?>" value="a">
+		<label class="custom-control-label" for="pregunta<?php echo $idpregunta;?>a">
+			si
+		</label>
 	</div>
 	<div class="custom-control custom-radio custom-control-inline">
-		<input type="radio" class="custom-control-input" id="pregunta_csej13b" name="pregunta_csej13" value="0">
-		<label class="custom-control-label" for="pregunta_csej13b">No</label>
+		<input type="radio" class="custom-control-input"
+			   id="pregunta<?php echo $idpregunta;?>b" name="pregunta<?php echo $idpregunta;?>" value="b">
+		<label class="custom-control-label" for="pregunta<?php echo $idpregunta;?>b">
+			no
+		</label>
 	</div>
 	<div class="custom-control custom-radio custom-control-inline">
-		<input type="radio" class="custom-control-input" id="pregunta_csej13c" name="pregunta_csej13" value="2">
-		<label class="custom-control-label" for="pregunta_csej13c">No se observaron casos</label>
+		<input type="radio" class="custom-control-input"
+			   id="pregunta<?php echo $idpregunta;?>c" name="pregunta<?php echo $idpregunta;?>" value="c">
+		<label class="custom-control-label" for="pregunta<?php echo $idpregunta;?>c">
+			no se observaron casos
+		</label>
 	</div>
 </div>
 <br>
